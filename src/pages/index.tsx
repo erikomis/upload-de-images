75389c9971a308e744562b0014e3b30b0f7c3c75
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-06-27 23:50:24 -0300

    fetchImage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,30 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  title:string
+  description:string
+  url:string
+  ts:number
+  id:string
+}
+
+interface GetImagesResponse {
+  after:string;
+  data:Image[]
+}
+
 export default function Home(): JSX.Element {
+
+  async function fetchImages ({pageParam=null}) :Promise<GetImagesResponse>{
+    const {data} = await api('/api/images',{
+      params: {
+        after:pageParam
+      }
+    })
+
+    return data
+  }
   const {
     data,
     isLoading,
@@ -18,6 +41,7 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
+
     // TODO AXIOS REQUEST WITH PARAM
     ,
     // TODO GET AND RETURN NEXT PAGE PARAM
