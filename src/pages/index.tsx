fbdabb1e465d14cb2c004fe9aff8078edb11381f
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-06-28 01:21:21 -0300

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -63,7 +63,7 @@ export default function Home(): JSX.Element {
     return <Loading />;
   }
 
-  if (isLoading && isError) {
+  if (!isLoading && isError) {
     return <Error />;
   }
   // TODO RENDER ERROR SCREEN
@@ -72,9 +72,14 @@ export default function Home(): JSX.Element {
     <>
       <Header />
 
-      <Box maxW={1120} px={20} mx="auto" my={20}>
+      <Box maxW={1120} px={[10, 15, 20]} mx="auto" my={[10, 15, 20]}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+
+        {hasNextPage && (
+          <Button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
