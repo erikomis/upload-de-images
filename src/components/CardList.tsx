583495bf4a9e7ed7a38664ba9d844c4d2c48cb15
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-06-28 00:47:35 -0300

    cardList

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,16 +16,32 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
+  const { onOpen, isOpen, onClose } = useDisclosure();
   // TODO MODAL USEDISCLOSURE
-
+  const [currentImageUrl, setCurrentImageUrl] = useState('');
   // TODO SELECTED IMAGE URL STATE
 
   // TODO FUNCTION HANDLE VIEW IMAGE
 
+  function handleViewImage(url: string): void {
+    onOpen();
+    setCurrentImageUrl(url);
+  }
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid column={[1, 2, 3]} spacing="40px">
+        {cards.map(card => (
+          // eslint-disable-next-line react/jsx-no-bind
+          <Card key={card.id} data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
 
+      <ModalViewImage
+        isOpen={isOpen}
+        imgUrl={currentImageUrl}
+        onClose={onClose}
+      />
       {/* TODO MODALVIEWIMAGE */}
     </>
   );
